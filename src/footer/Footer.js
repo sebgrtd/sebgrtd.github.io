e9dfa0f95484baae6badc4de2873f71a4472123b
Seb <smrsprite@gmail.com>
2022-02-17 02:58:04 +0100

    first

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -4,6 +4,6 @@ import { Container } from 'react-bootstrap'
 export default function Footer() {
   return <footer style={{width:'100%'}} className='text-center bg-dark py-4'>
         <h4 className='text-primary'>© 2022 Gratade Sébastien</h4>
-        <a className='text-secondary my-2'  href='#'>source</a>
+        <a className='text-secondary my-2'  href='https://github.com/sebgrtd/sebgrtd.github.io'>source</a>
       </footer>
 }
